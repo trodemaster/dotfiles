70237d9e50fd5cce8ec066e94f4fdec101c2ca7b
Blake Garner <blake@netjibbing.com>
2023-06-11 19:58:09 -0700

    macos testing

--- run_once_common.sh
+++ run_once_common.sh
@@ -10,4 +10,11 @@ if [ ! -d ~/code ]; then
     mkdir -p ~/code
 fi
 
+# config git
+git config --global push.default current
+git config --global pull.rebase false
+git config --global user.name "Blake Garner"
+git config --global user.email blake@netjibbing.com
+git config --global alias.change-commits '!'"f() { VAR=\$1; OLD=\$2; NEW=\$3; shift 3; git filter-branch --env-filter \"if [[ \\\"\$\`echo \$VAR\`\\\" = '\$OLD' ]]; then export \$VAR='\$NEW'; fi\" \$@; }; f"
+
 exit 0
