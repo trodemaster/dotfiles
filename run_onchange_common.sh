0e2de4fad43d42597a1351bc006489bb4d69e1e4
Blake Garner <blake@netjibbing.com>
2023-07-09 22:39:50 +0000

    improve git config

--- run_onchange_common.sh
+++ run_onchange_common.sh
@@ -11,11 +11,23 @@ if [ ! -d ~/code ]; then
 fi
 
 # config git
-git config --global push.default current
-git config --global pull.rebase false
-git config --global user.name "Blake Garner"
-git config --global user.email blake@netjibbing.com
-git config --global alias.change-commits '!'"f() { VAR=\$1; OLD=\$2; NEW=\$3; shift 3; git filter-branch --env-filter \"if [[ \\\"\$\`echo \$VAR\`\\\" = '\$OLD' ]]; then export \$VAR='\$NEW'; fi\" \$@; }; f"
-git config --unset commit.gpgsign
+if [ "$(git config --global push.default)" != "current" ]; then
+    git config --global push.default current
+fi
+if [ "$(git config --global pull.rebase)" != "false" ]; then
+    git config --global pull.rebase false
+fi
+if [ -z "$(git config --global user.name)" ]; then
+    git config --global user.name "Blake Garner"
+fi
+if [ -z "$(git config --global user.email)" ]; then
+    git config --global user.email blake@netjibbing.com
+fi
+if [ -z "$(git config --global alias.change-commits)" ]; then
+    git config --global alias.change-commits '!'"f() { VAR=\$1; OLD=\$2; NEW=\$3; shift 3; git filter-branch --env-filter \"if [[ \\\"\$\`echo \$VAR\`\\\" = '\$OLD' ]]; then export \$VAR='\$NEW'; fi\" \$@; }; f"
+fi
+if [ "$(git config --global commit.gpgsign)" != "false" ]; then
+    git config --global commit.gpgsign false
+fi
 
 exit 0
