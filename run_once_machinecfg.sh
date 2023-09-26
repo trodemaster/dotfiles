17a3a0becd8731895ee062981fc600369cb6c7ea
Blake Garner <blake@netjibbing.com>
2023-09-25 21:48:38 -0700

    de-templatize machineconfig

--- run_once_machinecfg.sh
+++ run_once_machinecfg.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
-# set -euo pipefail
-# IFS=$'\n\t'
-# shopt -s nullglob nocaseglob
+set -euo pipefail
+IFS=$'\n\t'
+shopt -s nullglob nocaseglob
 
 echo "machineconfig from chezmoi data is $MACHINECFG"
 
-exit 0
-
-# clone the repo from ghithib.com using the gh command line tool
-if [[ $MACHINE == "personal" ]]; then
+# clone the repo from github.com using the gh command line tool
+if [[ $MACHINECFG == "personal" ]]; then
   echo "cloning personal machine-cfg repo..."
-  if [ -d ~/code/machine-cfg ]; then
+  if [[ -d ~/code/machine-cfg ]]; then
     echo "machine-cfg repo already exists"
-    cd ~/code/machine-cfg
-    git pull
+    git -C ~/code/machine-cfg pull
   else
     git clone https://github.com/trodemaster/machine-cfg.git ~/code/machine-cfg
   fi
-elif [[ $MACHINE == "work" ]]; then
+fi
+
+exit 0
+
+if [[ $MACHINE == "work" ]]; then
   echo "cloning work machine-cfg repo..."
   gh auth login https://github.com/adobe/blake-machine-cfg ~/code/machine-cfg
-#  git clone
 fi
 
 exit 0
