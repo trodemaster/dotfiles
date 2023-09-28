330c7056a601f763239e0ff0d37dec22f894bb59
Blake Garner <blake@netjibbing.com>
2023-09-27 22:13:01 -0700

    clean sonoma install tweaks

--- run_once_machinecfg.sh
+++ run_once_machinecfg.sh
@@ -5,6 +5,12 @@ shopt -s nullglob nocaseglob
 
 echo "machineconfig from chezmoi data is $MACHINECFG"
 
+# check for gh cli tool
+if ! command -v gh &>/dev/null; then
+  echo "gh command line not installed skipping machine-cfg repo clone"
+  exit 0
+fi
+
 # if running on linux set BROWSER env var to false
 if [[ $(uname) == "Linux" ]]; then
   export BROWSER=false
