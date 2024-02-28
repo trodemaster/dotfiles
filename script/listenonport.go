46d2f9ff86575cc0c7045a55208a50b1b5817a2f
Blake Garner <blake@netjibbing.com>
2024-02-28 12:37:10 -0800

    sync nextbook

---
+++ script/listenonport.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello")
+	})
+
+	http.ListenAndServe(":8000", nil)
+}
