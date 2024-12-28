61f9472bfb1b3c074c80499df2d7b68d8aeed6de
Blake Garner <blake@netjibbing.com>
2024-12-28 11:19:03 -0800

    bork

--- run_onchange_common.sh
+++ run_onchange_common.sh
@@ -10,11 +10,12 @@ if [ ! -d ~/code ]; then
     mkdir -p ~/code
 fi
 
+rm /etc/ssh/sshd_config.d/206-machine-cfg.conf || true
 # add ssh include if the directory exists
 if [ -d ~/code/machine-cfg ]; then
     if [ -d /etc/ssh/sshd_config.d ]; then
         if [ ! -f /etc/ssh/sshd_config.d/206-machine-cfg.conf ]; then
-            echo "Include ~/code/machine-cfg/ssh_config" | sudo tee /etc/ssh/sshd_config.d/206-machine-cfg.conf >/dev/null
+            echo "Include ${HOME}/code/machine-cfg/ssh_config" | sudo tee /etc/ssh/sshd_config.d/206-machine-cfg.conf >/dev/null
         fi
     fi
 fi
