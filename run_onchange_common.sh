56e78cc8f32aaa1d066984093d395364754b158b
Blake Garner <blake@netjibbing.com>
2025-01-10 14:48:37 -0800

    back to templ

--- run_onchange_common.sh
+++ run_onchange_common.sh
@@ -10,13 +10,4 @@ if [ ! -d ~/code ]; then
     mkdir -p ~/code
 fi
 
-# add ssh include if the directory exists
-if [ -d ~/code/machine-cfg ]; then
-    if [ -d /etc/ssh/sshd_config.d ]; then
-        if [ ! -f /etc/ssh/sshd_config.d/206-machine-cfg.conf ]; then
-            echo "Include ${HOME}/code/machine-cfg/ssh_config" | sudo tee /etc/ssh/ssh_config.d/206-machine-cfg.conf >/dev/null
-        fi
-    fi
-fi
-
 exit 0
